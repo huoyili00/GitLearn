f85d2d19846b6e40f92a96e4ca0ad52dac8aa9f6
Yili Huo <Yili.Huo@bentley.com>
2021-04-23 10:43:29 -0700

    test fetch and merge

--- File3.cpp
+++ File3.cpp
@@ -4,4 +4,6 @@ Another guy creates another file.
 Test if local has some changes, and remote also has some changes (this line added in working).
 
   This time test push, when local has new also remote has new (this line added in GitHub, i.e. remote).
+  
+  write a line in local. Added to "local index", but not commited.
 
