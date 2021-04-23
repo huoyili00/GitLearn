b0c694807071f0b2ba7ab36676357f76c0cd2c83
huoyili00 <huoyili00@gmail.com>
2021-04-23 10:13:45 -0700

    test git diff

--- File4.h
+++ File4.h
@@ -6,3 +6,5 @@ This time tests merge (this line added in working space).
   This time test conflict (this line added in github, i.e. remote). 
 
   Add a line in GitHub.
+  
+  Add another line in GitHub.
